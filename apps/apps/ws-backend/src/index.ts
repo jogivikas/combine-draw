48883efc98a2ddc5cd4d7037870567d027b79227
vikas jogi <jogivikas666@gmail.com>
2025-08-28 13:39:11 +0530

    wsand http backend

--- apps/apps/ws-backend/src/index.ts
+++ apps/apps/ws-backend/src/index.ts
@@ -3,7 +3,6 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", function connection(ws) {
-    
   ws.on("message", function message(message) {
     ws.send("pong");
   });
